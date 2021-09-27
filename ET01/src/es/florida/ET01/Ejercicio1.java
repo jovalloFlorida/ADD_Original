e69fb3e4769f5353521f392351fabb6093f8fb8a
Jose Valverde <jovall@floridauniversitaria.es>
2021-09-27 18:50:03 +0200

    Ejercicio1

---
+++ ET01/src/es/florida/ET01/Ejercicio1.java
@@ -0,0 +1,11 @@
+package es.florida.ET01;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		System.out.println("Ejercicio 1");
+		String directorio = args[0];
+		System.out.println("Directorio: " + directorio);
+
+	}
+}
